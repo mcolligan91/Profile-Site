e7e05a24db532da3eaa25f5c1119b169bb96a205
mcolligan91 <60613289+mcolligan91@users.noreply.github.com>
2024-05-22 13:54:33 -0400

    Skills icons updates

--- src/Components/Skills/Skills.jsx
+++ src/Components/Skills/Skills.jsx
@@ -22,18 +22,28 @@ const Skills = (props) => {
 		{iconClass: 'microsoftsqlserver-plain', text: 'SQL Server'},
 		{iconClass: 'csharp-plain', text: 'C#'},
 		{iconClass: 'dot-net-plain', text: '.NET'},
-		{iconClass: 'git-plain', text: 'Git'}
+		{iconClass: 'git-plain', text: 'Git'},
+		{iconClass: 'selenium-original', text: 'Selenium'},
+		{iconClass: 'xd-plain', text: 'XD'},
+		{iconClass: 'photoshop-plain', text: 'Photoshop'}
 	];
 
-	const createIconRows = (array, chunkSize) => {
-		let rows = [];
-		for (let i = 0; i < array.length; i += chunkSize) {
-			rows.push(array.slice(i, i + chunkSize));
-		}
-		return rows;
-	}
+	const bioParagraph = (
+		<p className={`bio-text ${isInverted ? 'bio-text-inverted' : ''}`}>
+			Hi, I'm <span className='bio-text-highlight'>Michael</span>. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+		</p>
+	);
 
-	const skillsIcons = createIconRows(skillsIconsInfo, 6);
+	const renderSkillsIcons = () => (
+		skillsIconsInfo.map((info, i) => (
+			<Fade key={i} bottom duration={1250} distance='100px'>
+				<Label className='skills-icon-container' size='large'>
+					<i className={`icon skills-icon devicon-${info.iconClass}`} /> 
+					{info.text}                                    
+				</Label>
+			</Fade>
+		))
+	);
 
 	return (
 		<Container textAlign='center'>
@@ -42,33 +52,15 @@ const Skills = (props) => {
 				<Grid.Row>
 					<Grid.Column computer={12} tablet={14} mobile={14}>
 						<Fade bottom duration={1000} distance='50px'>
-							<p className={`bio-text ${isInverted ? 'bio-text-inverted' : ''}`}>Hi, I'm <span className='bio-text-highlight'>Michael</span>. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+							{bioParagraph}
+						</Fade>
+						<Fade bottom duration={1000} distance='50px'>
+							<div className='skills-icons-container'>
+								{renderSkillsIcons()}
+							</div>
 						</Fade>
 					</Grid.Column>
 				</Grid.Row>
-				<Fade bottom duration={1250} distance='50px'>
-					{skillsIcons.map((row, rowIndex) => (
-						<Grid.Row key={rowIndex}>
-							<div className='about-section-icons-container'>
-								{row.map((info, i) => {
-									const {iconClass, text} = info;
-									return (
-										<Fade key={i} bottom duration={1000 + (rowIndex * 6 + i) * 100} distance='100px'>
-											<div className='skills-icon-container'>
-												<div className='icon-label-row-top'>
-													<i className={`devicon-${iconClass} skills-icon`}></i>
-												</div>
-												<div className='icon-label-row icon-label-text'>
-													{text}
-												</div>
-											</div>
-										</Fade>
-									)
-								})}
-							</div>
-						</Grid.Row>
-					))}
-				</Fade>
 			</Grid>
 		</Container>
 	);
