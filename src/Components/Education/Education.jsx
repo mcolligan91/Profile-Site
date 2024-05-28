ff9a14cf9b23360446bcee861a40b6d1250b0de1
mcolligan91 <60613289+mcolligan91@users.noreply.github.com>
2024-05-27 22:27:17 -0400

    Education section image transitions

--- src/Components/Education/Education.jsx
+++ src/Components/Education/Education.jsx
@@ -1,77 +1,65 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Container, Card } from 'semantic-ui-react';
 import { Fade } from 'react-reveal';
 import { connect } from 'react-redux';
-
 import SectionHeader from '../Shared/SectionHeader/SectionHeader';
-
 import './Education.scss';
 
 const Education = (props) => {
-    const { isMobile, isInverted } = props;
-
-    const ufDescription = (
-        <span>I graduated Summa Cum Laude from the University of Florida with a Bachelor of Science in <a className='school-description-link' href={'https://catalog.ufl.edu/UGRD/colleges-schools/UGDCP/SUB_BSUB_BSUB01/'} target='_blank' rel='noopener noreferrer'>Sustainability and the Built Environment</a>. My thesis was about converting biogas from organic waste into renewable electricity. In addition to my studies, I was an active member of the Chi Phi Fraterntiy.</span>
-    )
+    const {isMobile, isInverted} = props;
 
     const educationData = [
         {
             school: 'University of Florida',
             date: 'June 2010 - May 2015',
             degree: 'B.S in Sustainability and the Built Environment',
-            description: ufDescription,
-            image: './UF.jpg'
+            description: (
+                <span>I graduated Summa Cum Laude from the University of Florida with a Bachelor of Science in <a className='school-description-link' href={'https://catalog.ufl.edu/UGRD/colleges-schools/UGDCP/SUB_BSUB_BSUB01/'} target='_blank' rel='noopener noreferrer'>Sustainability and the Built Environment</a>. My thesis was about converting biogas from organic material into renewable electricity. In addition to my studies, I was an active member of the Chi Phi Fraternity.</span>
+            ),
+            images: ['./UF_Campus_1.jpg', './UF_Campus_2.jpg', './UF_Campus_3.jpg', './UF_Campus_4.jpg', './UF_Campus_5.jpg', './UF_Campus_6.jpg']
         }
     ];
 
-    const [imageToggles, setImageToggles] = useState(Array(educationData.length).fill(false));
+    const [currentImageIdx, setCurrentImageIdx] = useState(Array(educationData.length).fill(0));
 
-    const handleImageClick = (index, event) => {
-        event.stopPropagation(); 
-        if (isMobile) {
-            setImageToggles(prevState => {
-                const newToggles = [...prevState];
-                newToggles[index] = !newToggles[index];
-                return newToggles;
-            });
-        }
-    };
+    useEffect(() => {
+        const intervals = educationData.map((edu, i) => {
+            return setInterval(() => {
+                setCurrentImageIdx(prevIndexes => {
+                    const newIndexes = [...prevIndexes];
+                    newIndexes[i] = (newIndexes[i] + 1) % edu.images.length;
+                    return newIndexes;
+                });
+            }, 5000);
+        });
 
-    const handleTouchStart = (index, event) => {
-        if (isMobile) {
-            setImageToggles(prevState => {
-                const newToggles = [...prevState];
-                newToggles[index] = !newToggles[index];
-                return newToggles;
-            });
-        }
-    };
+        return () => intervals.forEach(interval => clearInterval(interval));
+    }, [educationData]);
 
-    const handleOutsideClick = () => {
-        setImageToggles(Array(educationData.length).fill(false));
-    };
-    
     const renderSchoolCards = () => (
         educationData.map((edu, i) => (
             <Fade bottom duration={1250} distance='50px' key={i}>
-                    <div className='college-info-container'>
-                        <Card className='school-card'>
-                            <div className={`school-image-container ${imageToggles[i] ? 'toggled' : ''}`} onClick={(e) => handleImageClick(i, e)} onTouchStart={(e) => handleTouchStart(i, e)}>
-                                <img src={edu.image} alt={`${edu.school} Campus`} className='school-image' />
-                            </div>
-                            <Card.Content>
-                                <Card.Header content={edu.school} />
-                                <Card.Meta content={edu.date} />
-                                <Card.Description content={edu.description} />
-                            </Card.Content>
-                        </Card>
-                    </div>
+                <div className='college-info-container'>
+                    <Card className='school-card'>
+                        <div className='school-image-container'>
+                            {edu.images.map((image, j) => (
+                                <img key={j} src={image} alt={`${edu.school} Campus`} className={`school-image ${j === currentImageIdx[i] ? 'active' : ''}`}
+                                />
+                            ))}
+                        </div>
+                        <Card.Content>
+                            <Card.Header content={edu.school} />
+                            <Card.Meta content={edu.date} />
+                            <Card.Description content={edu.description} />
+                        </Card.Content>
+                    </Card>
+                </div>
             </Fade>
         ))
     );
 
     return (
-        <Container className='content-row-container' onClick={handleOutsideClick} onTouchStart={handleOutsideClick}>
+        <Container className='content-row-container'>
             <SectionHeader content='Education' />
             {renderSchoolCards()}
         </Container>
