30f7d84ab5916ae67c63477e7351835f3f463e01
mcolligan91 <60613289+mcolligan91@users.noreply.github.com>
2024-05-30 21:48:39 -0400

    Minor top nav hover styling update

--- src/Components/Shared/LightModeIconMenuItem/LightModeIconmenuItem.jsx
+++ src/Components/Shared/LightModeIconMenuItem/LightModeIconmenuItem.jsx
@@ -4,8 +4,8 @@ import { connect } from 'react-redux';
 
 const LightModeIconMenuItem = ({isInverted, className, clickFunction, mouseEnterFunction, mouseLeaveFunction, icon}) => {
     return (
-        <Menu.Item className={className}>
-            <div className={`top-nav-link-text ${isInverted ? 'top-nav-link-text-inverted' : ''}`} onClick={clickFunction} onMouseEnter={mouseEnterFunction} onMouseLeave={mouseLeaveFunction}>
+        <Menu.Item className={`light-mode-icon-menu-item ${className}`} onClick={clickFunction}>
+            <div className={`top-nav-link-text ${isInverted ? 'top-nav-link-text-inverted' : ''}`} onMouseEnter={mouseEnterFunction} onMouseLeave={mouseLeaveFunction}>
                 {icon}
             </div>
         </Menu.Item>
--- src/Components/TopNav/TopNav.jsx
+++ src/Components/TopNav/TopNav.jsx
@@ -6,7 +6,7 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faCircleHalfStroke } from '@fortawesome/free-solid-svg-icons';
 
 import SiteLogo from '../Shared/SiteLogo/SiteLogo';
-import LightModeIconMenuItem from '../Shared/LightModeIconMenuItem/LightModeIconmenuItem';
+import LightModeIconMenuItem from '../Shared/LightModeIconMenuItem/LightModeIconMenuItem';
 import DropdownMenu from '../Shared/DropdownMenu/DropdownMenu';
 
 import './TopNav.scss';
